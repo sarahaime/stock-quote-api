6736de8111964395e5439ee2e0a638b0318a3fc6
Sarahaime Rodriguez <sarardz095@gmail.com>
2022-03-26 10:07:35 -0400

    Api-service basic structure and test added

---
+++ api-service/src/userAuthentication/user-authentication-service.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+describe('User Authentication Service Tests', () => {
+
+  describe('#generateRandomPassword()',  () => {
+    it('password len should be 32',  () => {
+      assert.equal(32, 32);
+    });
+  });
+
+  describe('#registerUser()',  () => {
+    it('Valid registration',  () => {
+      assert.equal(-1, -1);
+    });
+  });
+
+
+
+});
